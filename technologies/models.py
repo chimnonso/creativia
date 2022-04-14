ad9abd66240ff8bb600347201c4a88ff23a27370
chimnonso <gabriel4amaizu@gmail.com>
2022-04-14 12:57:55 +0900

    modified technologies models

--- technologies/models.py
+++ technologies/models.py
@@ -5,12 +5,15 @@ from datetime import datetime
 
 # Create your models here.
 
+def default_list():
+    return ['Add first detail']
+
 class Technology(models.Model):
     title = models.CharField("Technology Name", max_length=150)
     short_description = models.CharField("Short Description", max_length=250)
     description = models.TextField("Description")
     date_posted = models.DateTimeField("Date Posted", default=datetime.now)
-    details_list = ArrayField(models.CharField(max_length=400), blank=True, null=True)
+    details_list = ArrayField(models.CharField(max_length=400), blank=False, null=False, default=default_list)
     photo = models.ImageField("Upload Image", upload_to='photo/%Y/%m/%d', height_field=None, width_field=None, max_length=None)
 
     class Meta:
