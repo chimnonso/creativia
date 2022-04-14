37140fb45667abf4dfeab5bf99da22401a26e862
chimnonso <gabriel4amaizu@gmail.com>
2022-04-14 11:56:03 +0900

    Update Requirements.txt

--- technologies/models.py
+++ technologies/models.py
@@ -10,7 +10,7 @@ class Technology(models.Model):
     short_description = models.CharField("Short Description", max_length=250)
     description = models.TextField("Description")
     date_posted = models.DateTimeField("Date Posted", default=datetime.now)
-    details_list = ArrayField(models.CharField(max_length=400), blank=False, null=False)
+    details_list = ArrayField(models.CharField(max_length=400), blank=True, null=True)
     photo = models.ImageField("Upload Image", upload_to='photo/%Y/%m/%d', height_field=None, width_field=None, max_length=None)
 
     class Meta:
