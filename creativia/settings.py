346ce4d954c2b681bdbc9683fc666e2a5afcb090
chimnonso <gabriel4amaizu@gmail.com>
2022-10-24 17:15:08 +0900

    fixing git

--- creativia/settings.py
+++ creativia/settings.py
@@ -158,14 +158,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'creativia/static'
+    BASE_DIR / 'static'
 ]
+
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
